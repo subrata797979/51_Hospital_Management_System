8c8979f2865e5d09b11c72604d1042b834cfaedb
subrata797979 <subrata797979@gmail.com>
2021-07-04 08:35:24 +0530

    re-structure

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+
+// Checking if patinet, doctor, employe, receptionist
+
+var users = [
+    { username: 'user1', password: 'pass1' },
+    { username: 'user2', password: 'pass2' },
+    { username: 'user3', password: 'pass3' }
+];
+
+var button = document.getElementById('login');
+
+button.onclick = function() {
+   var username = document.getElementById('username').value;
+   var password = document.getElementById('password').value; 
+
+   for (var i = 0; i < users.length; i++) {
+      if(username == users[i].username && password == users[i].password) {
+         window.location.href = 'http://where/you/want/to/redirect/';
+         break;
+      }else{
+         alert('You are trying to break in!');
+      }
+   }
+}
