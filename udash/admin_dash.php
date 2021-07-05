136142d0490241b24de0dcb7f884b4df6598d64f
subrata797979 <subrata797979@gmail.com>
2021-07-05 20:21:39 +0530

    Sidebar

---
+++ udash/admin_dash.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+//for hiding errors
+error_reporting(E_PARSE | E_ERROR);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashborad</title>
+</head>
+<body>
+    
+</body>
+</html>
--- udash/doctor_dash.php
+++ udash/doctor_dash.php
@@ -10,16 +10,24 @@ error_reporting(E_PARSE | E_ERROR);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- css files -->
+    <link rel="stylesheet" href="../css/custom.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/dark-mode.css">
+
     <title>Doctor Dashboard</title>
 </head>
 <body>
-    <h1>This is doctor dashboard</h1>
+    <div include-html="../sidebar.php" id="sidebar"></div>
     <p>
-        Id : 
-        <?php
-                $id=$_SESSION['id'];
-                echo $id;
-        ?>
+        I m body
     </p>
+
+    <script src="../js/custom.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/dark-mode-switch.min.js"></script>
+    <script src="../js/jquery-3.6.0.min.js"></script>
+    <script src="../js/include-html.min.js"></script>
 </body>
 </html>
