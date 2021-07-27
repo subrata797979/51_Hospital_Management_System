f75fc47663a49b413efc4d096a40fc97b47cce3e
subrata797979 <subrata797979@gmail.com>
2021-07-27 17:33:13 +0530

    all done dash

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once __DIR__ . '/vendor/autoload.php';
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-
-    var_dump(getenv('LOCALHOST'));
-    var_dump($_ENV['LOCALHOST']);
-?>
